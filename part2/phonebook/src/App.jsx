8f7f59b89a85a3e3a1c4c06eec0f7b8c03eac776
Oleksii <albulgyn@gmail.com>
2026-09-14 15:10:59 +0300

    2.15

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -65,8 +65,22 @@ const App = () => {
   const addPerson = (event) => {
     event.preventDefault()
 
-    if (persons.some(person => person.name === newName)) { // check ORs
-      alert(`${newName} is already added to phonebook`)
+    const existingPerson = persons.find(person => person.name === newName)
+
+    if (existingPerson) {
+      if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
+        const updatedPerson = { ...existingPerson, number: newNumber }
+
+        personService
+          .update(existingPerson.id, updatedPerson)
+          .then(returnedPerson => {
+            setPersons(persons.map(person =>
+              person.id === existingPerson.id ? returnedPerson : person
+            ))
+            setNewName('')
+            setNewNumber('')
+          })
+      }
       return
     }
 
