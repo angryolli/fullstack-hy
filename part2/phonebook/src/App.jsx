3fec9682e8c2422acbc6363be077166a8c66c336
Oleksii <albulgyn@gmail.com>
2026-09-14 14:32:49 +0300

    2.12

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -67,13 +67,16 @@ const App = () => {
 
     const personObject = {
       name: newName,
-      number: newNumber,
-      id: persons.length + 1
+      number: newNumber
     }
 
-    setPersons(persons.concat(personObject))
-    setNewName('')
-    setNewNumber('')
+    axios
+      .post('http://localhost:3001/persons', personObject)
+      .then(response => {
+        setPersons(persons.concat(response.data))
+        setNewName('')
+        setNewNumber('')
+      })
   }
 
   const handleNameChange = (event) => {
