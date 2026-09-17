f77d8430d4c70abb390b1c2efce5e1c011102a22
Oleksii <albulgyn@gmail.com>
2026-09-17 13:26:59 +0300

    3.15

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -69,11 +69,12 @@ app.get('/info', (request, response) => {
   )
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
